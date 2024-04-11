d3500721dc9f362fbe63fbdaa0825341ccf9ffad
Rominks <151865395+Rominks@users.noreply.github.com>
2024-04-11 11:30:05 +0300

    locale setup + autogenerated files

---
+++ lang/en/app.php
@@ -0,0 +1,13 @@
+<?php
+
+// lang/en/app.php
+
+return [
+    'submit' => 'Submit',
+    'delete' => 'Delete',
+    'home_module' => [
+        'index_title' => 'Home Page',
+        'contact_title' => 'Contact Page',
+        'welcome' => 'Welcome, :name'
+    ],
+];
---
+++ lang/lt/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'submit' => 'Pateikti',
+    'delete' => 'Ištrinti',
+    'home_module' => [
+        'index_title' => 'Pagrindinis',
+        'contact_title' => 'Kontaktai',
+        'welcome' => 'Sveiki, :name'
+    ],
+];
