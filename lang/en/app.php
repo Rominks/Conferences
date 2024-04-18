08eabcf070b6b668cf85c0999618195a8ab1955f
Rominks <151865395+Rominks@users.noreply.github.com>
2024-04-18 15:18:37 +0300

    new translations

--- lang/en/app.php
+++ lang/en/app.php
@@ -5,9 +5,38 @@
 return [
     'submit' => 'Submit',
     'delete' => 'Delete',
+    'edit' => 'Edit',
+    'close' => 'Close',
+    'create' => 'Create',
+    'remember_me' => 'Remember me',
+    'edit_article' => 'Edit Article',
+    'create_article' => 'Create Article',
+    'nav_bar' => [
+        'login' => 'Login',
+        'logout' => 'Logout',
+        'cms' => 'Article CMS',
+        'home' => 'Home'
+    ],
     'home_module' => [
         'index_title' => 'Home Page',
         'contact_title' => 'Contact Page',
-        'welcome' => 'Welcome, :name'
+        'welcome' => 'Welcome, :name',
     ],
+    'article_model' => [
+        'title' => 'Title',
+        'address' => 'Address',
+        'attendance' => 'Attendance',
+        'date' => 'Date',
+        'content' => 'Content',
+        'created_at' => 'Created',
+        'updated_at' => 'Updated'
+    ],
+    'user_model' => [
+        'username' => 'Username',
+        'password' => 'Password',
+    ],
+    'cms_module' => [
+        'cms_title' => 'Content Management System',
+        'actions' => 'Actions',
+    ]
 ];
--- lang/lt/app.php
+++ lang/lt/app.php
@@ -3,9 +3,38 @@
 return [
     'submit' => 'Pateikti',
     'delete' => 'Ištrinti',
-    'home_module' => [
-        'index_title' => 'Pagrindinis',
-        'contact_title' => 'Kontaktai',
-        'welcome' => 'Sveiki, :name'
+    'edit' => 'Redaguoti',
+    'remember_me' => 'Prisiminti mane',
+    'close' => 'Uždaryti',
+    'create' => 'Pridėti',
+    'edit_article' => 'Redaguoti konferenciją',
+    'create_article' => 'Sukurti konferenciją',
+    'nav_bar' => [
+        'login' => 'Prisijungti',
+        'logout' => 'Atsijungti',
+        'cms' => 'Konferencijų TVS',
+        'home' => 'Pagrindinis',
     ],
+    'home_module' => [
+        'index_title' => 'Pagrindinis puslapis',
+        'contact_title' => 'Kontaktai',
+        'welcome' => 'Sveiki, :name',
+    ],
+    'article_model' => [
+        'title' => 'Pavadinimas',
+        'address' => 'Adresas',
+        'attendance' => 'Dalyvaujančių kiekis',
+        'date' => 'Data',
+        'content' => 'Aprašas',
+        'created_at' => 'Sukurtas',
+        'updated_at' => 'Atnaujintas'
+    ],
+    'user_model' => [
+        'username' => 'Vartotojo vardas',
+        'password' => 'Slaptažodis',
+    ],
+    'cms_module' => [
+        'cms_title' => 'Turinio valdymo sistema',
+        'actions' => 'Veiksmai',
+    ]
 ];
